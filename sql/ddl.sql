93a08df5c4aabda1cb6d9ba588c82e1180bf2872
xipian <xipian@foxmail.com>
2023-10-14 00:39:05 +0800

    add 修正 ddl.sql文件

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -23,6 +23,10 @@ create table if not exists user
     unique (userAccount)
     ) comment '用户';
 
+INSERT INTO `user` VALUES ('1', 'gaomu', 'gaomu', null, null, 'user', '78e99c2b820b83b2fbb64dbccb1311b5', 'fb0e8ba8cae5fdef8d321191517e61ec', '80d0d0daef59758438523e6556e13036', '2023-10-12 21:34:14', '2023-10-13 03:24:54', '0');
+INSERT INTO `user` VALUES ('2', null, 'admin', null, null, 'admin', '78e99c2b820b83b2fbb64dbccb1311b5', '9d868b9eb9148a8fee73b4c7d3ddf2c0', '74a7e15239426151b862f77b55d545a7', '2023-10-13 03:44:11', '2023-10-13 03:44:36', '0');
+
+
 -- 接口信息
 create table if not exists emonapi.`interface_info`
 (
@@ -41,6 +45,20 @@ create table if not exists emonapi.`interface_info`
     `isDelete` tinyint default 0 not null comment '是否删除(0-未删, 1-已删)'
     ) comment '接口信息';
 
+
+INSERT INTO `interface_info` VALUES ('1', '测试数据-无法使用', 'LN0G', 'www.newton-rolfson.com', '测试', '测试', '测试', '0', 'GET', '1', '2023-10-13 15:02:17', '2023-10-13 15:02:29', '0');
+INSERT INTO `interface_info` VALUES ('2', 'getUsernameByPost', '获取你的名字', 'http://localhost:8123/api/name/user', '[\n    {\"name\": \"username\", \"type\": \"string\"}\n]', '{\"Content-Type\": \"application/json\"}', '{\"Content-Type\": \"application/json\"}', '1', 'POST', '2', '2023-10-06 19:26:18', '2023-10-13 03:07:27', '0');
+INSERT INTO `interface_info` VALUES ('3', '测试数据-无法使用', 'H44H', 'www.hyon-gorczany.co', '测试', '测试', '测试', '0', 'GET', '1', '2023-10-13 15:02:17', '2023-10-13 15:02:34', '0');
+INSERT INTO `interface_info` VALUES ('6', '测试数据-无法使用', '8R', 'www.mertie-abernathy.org', '测试', '测试', '测试', '0', 'GET', '1', '2023-10-13 15:02:17', '2023-10-13 15:02:17', '0');
+INSERT INTO `interface_info` VALUES ('7', '测试数据-无法使用', 'ft5Vx', 'www.ashlea-mosciski.net', '测试', '测试', '测试', '0', 'GET', '1', '2023-10-13 15:02:17', '2023-10-13 15:02:17', '0');
+INSERT INTO `interface_info` VALUES ('8', '测试数据-无法使用', 'aPagp', 'www.patricia-harris.com', '测试', '测试', '测试', '0', 'GET', '1', '2023-10-13 15:02:18', '2023-10-13 15:02:18', '0');
+INSERT INTO `interface_info` VALUES ('9', '测试数据-无法使用', 'sM2S', 'www.reid-greenfelder.net', '测试', '测试', '测试', '0', 'GET', '1', '2023-10-13 15:02:18', '2023-10-13 15:02:18', '0');
+INSERT INTO `interface_info` VALUES ('10', '测试数据-无法使用', 'tI', 'www.boyd-bernier.io', '测试', '测试', '测试', '0', 'GET', '1', '2023-10-13 15:02:18', '2023-10-13 15:02:18', '0');
+INSERT INTO `interface_info` VALUES ('11', '测试数据-无法使用', '8s', 'www.gaylord-heller.co', '测试', '测试', '测试', '0', 'GET', '1', '2023-10-13 15:02:18', '2023-10-13 15:02:18', '0');
+INSERT INTO `interface_info` VALUES ('12', '测试数据-无法使用', 'Hh8', 'www.chong-fritsch.info', '测试', '测试', '测试', '0', 'GET', '1', '2023-10-13 15:02:18', '2023-10-13 15:02:18', '0');
+INSERT INTO `interface_info` VALUES ('13', '测试数据-无法使用', 'Dj', 'www.lloyd-stiedemann.name', '测试', '测试', '测试', '0', 'GET', '1', '2023-10-13 15:02:18', '2023-10-13 15:02:18', '0');
+
+
 -- 用户调用接口关系表
 create table if not exists emonapi.`user_interface_info`
 (
@@ -54,3 +72,11 @@ create table if not exists emonapi.`user_interface_info`
     `updateTime` datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
     `isDelete` tinyint default 0 not null comment '是否删除(0-未删, 1-已删)'
 ) comment '用户调用接口关系';
+
+INSERT INTO `user_interface_info` VALUES ('1', '1', '1', '23', '7', '0', '2023-10-12 00:42:29', '2023-10-12 16:51:25', '0');
+INSERT INTO `user_interface_info` VALUES ('2', '3', '1', '0', '100000', '0', '2023-10-12 03:15:20', '2023-10-12 03:15:20', '0');
+INSERT INTO `user_interface_info` VALUES ('3', '2', '2', '3', '99999', '0', '2023-10-12 04:31:27', '2023-10-12 04:42:29', '0');
+INSERT INTO `user_interface_info` VALUES ('4', '3', '2', '18', '99995', '0', '2023-10-12 04:31:53', '2023-10-13 03:49:05', '0');
+INSERT INTO `user_interface_info` VALUES ('5', '2', '3', '3', '3', '0', '2023-10-12 04:32:04', '2023-10-12 04:42:40', '0');
+INSERT INTO `user_interface_info` VALUES ('6', '3', '3', '30', '10000', '0', '2023-10-12 04:32:27', '2023-10-12 04:32:27', '0');
+INSERT INTO `user_interface_info` VALUES ('7', '4', '2', '6', '99994', '0', '2023-10-13 03:24:08', '2023-10-13 03:43:12', '0');
